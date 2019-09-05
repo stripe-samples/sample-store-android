20e5e45ef49130a86bb7a4c2464d44392e2e1fd1
Michael Shafrir <mshafrir@stripe.com>
2019-09-05 12:41:10 -0400

    Migrate to AndroidX

--- app/src/main/java/com/stripe/samplestore/ItemDivider.kt
+++ app/src/main/java/com/stripe/samplestore/ItemDivider.kt
@@ -3,9 +3,9 @@ package com.stripe.samplestore
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
-import android.support.annotation.DrawableRes
-import android.support.v4.content.ContextCompat
-import android.support.v7.widget.RecyclerView
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Custom divider will be used in the list.
