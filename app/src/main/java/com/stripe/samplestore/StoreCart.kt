42cbd353b5acef54ce582b654cd10d97e6e426d4
Michael Shafrir <mshafrir@stripe.com>
2020-05-19 10:41:31 -0400

    Cleanup StoreAdapter and related classes
    - Remove ability to specify a price multiplier - backend is ultimate
      source of truth for prices
    - Simplify `StoreCart`
    - Add `StoreCartTest`

--- app/src/main/java/com/stripe/samplestore/StoreCart.kt
+++ app/src/main/java/com/stripe/samplestore/StoreCart.kt
@@ -3,22 +3,13 @@ package com.stripe.samplestore
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import java.util.Currency
-import java.util.UUID
 
 @Parcelize
-data class StoreCart(
+internal data class StoreCart(
     val currency: Currency,
-    val storeLineItems: MutableMap<String, StoreLineItem> = mutableMapOf()
+    val lineItems: List<StoreLineItem>
 ) : Parcelable {
-    internal val lineItems: List<StoreLineItem>
-        get() = storeLineItems.values.toList()
 
     internal val totalPrice: Long
-        get() = storeLineItems.values.sumBy { it.totalPrice.toInt() }.toLong()
-
-    fun addStoreLineItem(description: String, quantity: Int, unitPrice: Long) {
-        storeLineItems[UUID.randomUUID().toString()] = StoreLineItem(
-            description, quantity, unitPrice
-        )
-    }
+        get() = lineItems.sumBy { it.totalPrice.toInt() }.toLong()
 }
--- app/src/main/java/com/stripe/samplestore/StoreLineItem.kt
+++ app/src/main/java/com/stripe/samplestore/StoreLineItem.kt
@@ -7,12 +7,20 @@ import kotlinx.android.parcel.Parcelize
  * Represents a single line item for purchase in this store.
  */
 @Parcelize
-data class StoreLineItem(
+internal data class StoreLineItem(
     val description: String,
     val quantity: Int,
     val unitPrice: Long,
     val isProduct: Boolean = true
 ) : Parcelable {
+
+    constructor(product: Product, quantity: Int) : this(
+        product.emoji,
+        quantity,
+        product.price.toLong(),
+        true
+    )
+
     internal val totalPrice: Long
         get() = unitPrice * quantity
 }
