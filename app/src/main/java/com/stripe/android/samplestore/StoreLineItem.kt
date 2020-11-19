23cc38b45b46d76ec6aec14a64efdefa807af40b
Michael Shafrir <45020849+mshafrir-stripe@users.noreply.github.com>
2020-11-19 14:03:06 -0500

    Update dependencies (#65)
    - Upgrade Android Gradle Plugin to 4.1.1
    - Upgrade Gradle to 6.7.1
    - Upgrade Kotlin to 1.4.20
    - Upgrade Kotlin Coroutines to 1.4.1
    - Upgrade build tools to 30.0.2
    - Replace `kotlin-android-extensions` with Kotlin Parcelize plugin

--- app/src/main/java/com/stripe/android/samplestore/StoreLineItem.kt
+++ app/src/main/java/com/stripe/android/samplestore/StoreLineItem.kt
@@ -1,7 +1,7 @@
 package com.stripe.android.samplestore
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 /**
  * Represents a single line item for purchase in this store.
